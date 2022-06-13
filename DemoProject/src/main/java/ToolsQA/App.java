5e356b9e762ddba088b02858d8d29dddf49bd26d
Guntupallisindhujaa <sindhujaa.sindhu1998@gmail.com>
2022-06-13 11:23:15 +0530

    Update From Sindhu

--- DemoProject/src/main/java/ToolsQA/App.java
+++ DemoProject/src/main/java/ToolsQA/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-         System.out.println( "Hello World! -- new chnages" );
+         System.out.println( "Hello World! -- new changes" );
+        System.out.println( "Hello World! -- new changes from Sindhujaa" );
     }
 }
